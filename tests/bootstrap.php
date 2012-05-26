0d50ced57b715e58088d9222033302708c665e51
Ka Yue Yeung <kayuey@gmail.com>
2012-05-26 10:46:36 +0800

    Add comments.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,16 @@
 <?php
 
 $GLOBALS['wp_tests_options'] = array(
-    'active_plugins' => array( 'hello.php' ),
+    // parent theme.
     'template' => 'twentyeleven',
+
+    // child theme, set to the same as parent theme if not a child theme.
     'stylesheet' => 'twentytest',
+
+    // activate plugins.
+    'active_plugins' => array(
+        'hello.php',
+    ),
 );
 
 $path = './vendors/WordPressUnitTest/init.php';
